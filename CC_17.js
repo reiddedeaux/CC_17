d9758369398fe178b2546af59c04c10d938d1258
reiddedeaux <rdedeaux@usf.edu>
2025-03-30 22:55:42 -0400

    Task 2 - Created SalesRep Class

--- CC_17.js
+++ CC_17.js
@@ -14,3 +14,19 @@ class Customer {
         return this.purchaseHistory.reduce((total, amt)=> total + amt, 0);
     }
 }
+//Task 2
+class SalesRep {  // Class for sales reps
+    constructor(name){
+        this.name = name //name
+        this.clients = [];//clients
+    }
+    addClient(customer){ // adds a customer to list
+        this.clients.push(customer);
+    }
+    getClientTotal(name){ // how much money theyve spent
+        const client = this.clients.find(c => c.name === name);
+        if(client){
+            console.log(`Total amount ${client.name}: $${client.getTotalSpent()}`);            
+        }
+    }
+}
